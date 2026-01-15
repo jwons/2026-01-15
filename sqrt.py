3091f164f9538cf9cd60c3fd0494fa1a11cb4d92
Joseph Wonsil <joe.wonsil@gmail.com>
2026-01-15 13:19:47 -0800

    added a sqrt function

---
+++ sqrt.py
@@ -0,0 +1,17 @@
+# author: Tiffany Timbers
+# date: 2020-02-23
+
+"""Calculates and prints the square root of a given number."""
+
+import click
+import math
+
+@click.command()
+@click.option("--n", type=int, required=True, help="Number for which the square root should be calculated")
+def main(n):
+    if n < 0:
+        raise Exception("n should be a positive number")
+    print(math.sqrt(n))
+
+if __name__ == "__main__":
+    main()
